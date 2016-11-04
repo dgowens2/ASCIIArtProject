98a8fa7af8bed7ad0b798639fa39864b71ad3422
dgowens2 <dgowens2@gmail.com>
2016-11-04 13:45:26 -0400

    my first app

---
+++ AsciiDrawingHomework.java
@@ -0,0 +1,266 @@
+public class AsciiDrawingHomework {
+	public static void main(String[] args) throws Exception {
+		long sleepAmount = 1000;
+
+		System.out.println("Go Braves!!");
+
+		Thread.sleep(sleepAmount);
+
+		System.out.println("");
+
+		System.out.println("");
+	
+		System.out.println("Ohhhh oh ohhhhhhhhh!!!");
+
+		Thread.sleep(sleepAmount);
+	
+		clearWindowsScreen();
+
+		System.out.println("");
+		System.out.println("");	
+		System.out.println("                                   /\\");
+		System.out.println("                                  /  \\");
+		System.out.println("                                 /    \\");
+		System.out.println("                                /      \\");
+		System.out.println("                               /        \\");
+		System.out.println("                              /          \\");
+		System.out.println("                        (____/____________\\______");
+		System.out.println("                            /              \\     )");
+		System.out.println("                           /                \\");
+		System.out.println("                          /                  \\");
+		System.out.println("                         /                    \\");
+		System.out.println("                 |      /                      \\");
+		System.out.println("                  \\    /                        \\");
+		System.out.println("                   \\__/                          \\__/");
+
+		Thread.sleep(sleepAmount);
+		
+		clearWindowsScreen();
+
+		System.out.println("");
+		System.out.println("");	
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("                                                 /\\");
+		System.out.println("                                ________________/__\\");
+		System.out.println("                               (_|_____________/_X__\\");
+		System.out.println("                                              /      \\");
+		System.out.println("                                             /________\\");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+
+		Thread.sleep(sleepAmount);
+		
+		clearWindowsScreen();
+
+		System.out.println("");
+		System.out.println("");	
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("                                                        /\\");
+		System.out.println("                                                       /  \\");
+		System.out.println("                            __________________________/____\\");
+		System.out.println("                           (_|_______________________/_X__X_\\");
+		System.out.println("                                                    /        \\");
+		System.out.println("                                                   /          \\");
+		System.out.println("                                                  /____________\\");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+
+		Thread.sleep(sleepAmount);
+		
+		clearWindowsScreen();
+
+		System.out.println("");
+		System.out.println("");	
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("                                                          /\\");
+		System.out.println("                                                         /  \\");
+		System.out.println("                                                        /    \\");
+		System.out.println("                        _______________________________/______\\");
+		System.out.println("                       (_|____________________________/__X__X__\\");
+		System.out.println("                                                     /          \\");
+		System.out.println("                                                    /            \\");
+		System.out.println("                                                   /              \\");
+		System.out.println("                                                  /________________\\");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		
+
+
+		Thread.sleep(sleepAmount);
+
+		for (int i = 0; i < 5; i++) {	
+			
+		clearWindowsScreen();
+
+		System.out.println("");
+		System.out.println("");	
+		System.out.println("                                   /\\");
+		System.out.println("                                  /  \\");
+		System.out.println("                                 /    \\");
+		System.out.println("                                /      \\");
+		System.out.println("                               /        \\");
+		System.out.println("                              /          \\");
+		System.out.println("                             /            \\");
+		System.out.println("                            /              \\");
+		System.out.println("                           /                \\                     /\\");
+		System.out.println("                          /                  \\                   /  \\");
+		System.out.println("                         /                    \\                 /    \\");
+		System.out.println("                        /                      \\               /      \\");
+		System.out.println("                       /                        \\             /        \\");
+		System.out.println("              ________/__________________________\\___________/__________\\");
+		System.out.println("             (_|_____/____________________________\\_________/___X__X__X__\\");
+		System.out.println("                    /                              \\       /              \\");
+		System.out.println("                   /                                \\     /                \\");
+		System.out.println("                  /                                  \\   /                  \\");
+		System.out.println("                 /                                    \\ /____________________\\");
+		System.out.println("                /                                      \\");
+		System.out.println("               /                                        \\");
+		System.out.println("              /                                          \\");
+		System.out.println("             /                                            \\");
+		System.out.println("            /                                              \\");
+		System.out.println("           /                                                \\");
+		System.out.println("          /                                                  \\");
+		System.out.println(" |       /                                                    \\");
+		System.out.println("  \\     /                                                      \\");
+		System.out.println("   \\___/                                                        \\__/");
+
+		Thread.sleep(sleepAmount);
+		
+		clearWindowsScreen();
+
+		System.out.println("");
+		System.out.println("");	
+		System.out.println("                                   /\\");
+		System.out.println("                                  /  \\");
+		System.out.println("                                 /    \\");
+		System.out.println("                                /      \\");
+		System.out.println("                               /        \\");
+		System.out.println("                              /          \\");
+		System.out.println("                             /            \\");
+		System.out.println("                            /              \\");
+		System.out.println("                           /                \\");
+		System.out.println("                          /                  \\");
+		System.out.println("                         /                    \\                     /\\");
+		System.out.println("                        /                      \\                   /  \\");
+		System.out.println("                       /                        \\                 /    \\");
+		System.out.println("                      /                          \\               /      \\");
+		System.out.println("                     /                            \\             /        \\");
+		System.out.println("              ______/______________________________\\___________/__________\\");
+		System.out.println("             (_|___/________________________________\\_________/___X__X__X__\\");
+		System.out.println("                  /                                  \\       /              \\");
+		System.out.println("                 /                                    \\     /                \\");
+		System.out.println("                /                                      \\   /                  \\");
+		System.out.println("               /                                        \\ /____________________\\");
+		System.out.println("              /                                          \\");
+		System.out.println("             /                                            \\");
+		System.out.println("            /                                              \\");
+		System.out.println("           /                                                \\");
+		System.out.println("          /                                                  \\");
+		System.out.println(" |       /                                                    \\");
+		System.out.println("  \\     /                                                      \\");
+		System.out.println("   \\___/                                                        \\__/");
+
+		Thread.sleep(sleepAmount);
+		
+		clearWindowsScreen();
+
+		System.out.println("");
+		System.out.println("");	
+		System.out.println("                                   /\\");
+		System.out.println("                                  /  \\");
+		System.out.println("                                 /    \\");
+		System.out.println("                                /      \\");
+		System.out.println("                               /        \\");
+		System.out.println("                              /          \\");
+		System.out.println("                             /            \\");
+		System.out.println("                            /              \\");
+		System.out.println("                           /                \\                     /\\");
+		System.out.println("                          /                  \\                   /  \\");
+		System.out.println("                         /                    \\                 /    \\");
+		System.out.println("                        /                      \\               /      \\");
+		System.out.println("                       /                        \\             /        \\");
+		System.out.println("              ________/__________________________\\___________/__________\\");
+		System.out.println("             (_|_____/____________________________\\_________/___X__X__X__\\");
+		System.out.println("                    /                              \\       /              \\");
+		System.out.println("                   /                                \\     /                \\");
+		System.out.println("                  /                                  \\   /                  \\");
+		System.out.println("                 /                                    \\ /____________________\\");
+		System.out.println("                /                                      \\");
+		System.out.println("               /                                        \\");
+		System.out.println("              /                                          \\");
+		System.out.println("             /                                            \\");
+		System.out.println("            /                                              \\");
+		System.out.println("           /                                                \\");
+		System.out.println("          /                                                  \\");
+		System.out.println(" |       /                                                    \\");
+		System.out.println("  \\     /                                                      \\");
+		System.out.println("   \\___/                                                        \\__/");
+
+	}
+	}
+
+public static void clearWindowsScreen() { 
+        try { 
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor(); 
+        } catch (Exception exception) {
+            // intentionally don't do anything here ... 
+        }
+    } 
+}
